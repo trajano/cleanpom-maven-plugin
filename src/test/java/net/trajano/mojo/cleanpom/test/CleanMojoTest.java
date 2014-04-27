cf6b94b2b4f1cd8e02ae14629d2b18c4febf17c6
Archimedes Trajano <developer@trajano.net>
2014-04-26 21:20:19 -0400

    Corrected the property name
    Added a JUnit test to prove that the exception property is defined correctly.

--- src/test/java/net/trajano/mojo/cleanpom/test/CleanMojoTest.java
+++ src/test/java/net/trajano/mojo/cleanpom/test/CleanMojoTest.java
@@ -1,14 +1,19 @@
 package net.trajano.mojo.cleanpom.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.FileInputStream;
 
+import javax.xml.transform.TransformerException;
+
 import net.trajano.mojo.cleanpom.CleanMojo;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.codehaus.plexus.util.FileUtils;
 import org.junit.Rule;
@@ -40,12 +45,36 @@ public class CleanMojoTest {
     }
 
     /**
+     * Fail test because input was not XML.
+     *
+     * @throws Exception
+     */
+    @Test()
+    public void testNonPom() throws Exception {
+        final File defaultPom = new File(
+                "src/test/resources/net/trajano/mojo/cleanpom/default-pom.xml");
+        final File testPom = new File(
+                "src/main/resources/META-INF/Messages.properties");
+        final File tempPom = new File("target/test-pom.xml");
+        FileUtils.copyFile(testPom, tempPom);
+        assertTrue(tempPom.exists());
+
+        final CleanMojo mojo = (CleanMojo) rule.lookupMojo("clean", defaultPom);
+        try {
+            mojo.execute();
+            fail("Should not reach here.");
+        } catch (final MojoExecutionException e) {
+            assertEquals(TransformerException.class, e.getCause().getClass());
+        }
+    }
+
+    /**
      * Makes sure license is preserved.
      *
      * @throws Exception
      */
     @Test
-    public void testPomWitLicense() throws Exception {
+    public void testPomWithLicense() throws Exception {
         final File testPom = new File(
                 "src/test/resources/net/trajano/mojo/cleanpom/default-pom-with-license.xml");
         final File tempPom = new File("target/test-pom.xml");
