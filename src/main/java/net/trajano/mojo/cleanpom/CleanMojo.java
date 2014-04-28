1a05fde4f0071abb6d67b2a05142e11134ca8e98
Archimedes Trajano <developer@trajano.net>
2014-04-28 03:03:44 -0400

    Reduce complexity per method average
    Separated the temporary file build logic.  Separated tranformation.
    Used plexus-build-api to allow usage in other environments.

--- src/main/java/net/trajano/mojo/cleanpom/CleanMojo.java
+++ src/main/java/net/trajano/mojo/cleanpom/CleanMojo.java
@@ -4,9 +4,12 @@ import static java.lang.String.format;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ResourceBundle;
 
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
@@ -14,10 +17,12 @@ import javax.xml.transform.stream.StreamSource;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.codehaus.plexus.util.FileUtils;
+import org.sonatype.plexus.build.incremental.BuildContext;
 
 /**
  * Cleans the POM.
@@ -29,6 +34,13 @@ public class CleanMojo extends AbstractMojo {
      */
     private static final ResourceBundle R = ResourceBundle
             .getBundle("META-INF/Messages");
+
+    /**
+     * Build context.
+     */
+    @Component
+    private BuildContext buildContext;
+
     /**
      * The POM file to update.
      */
@@ -36,9 +48,34 @@ public class CleanMojo extends AbstractMojo {
     private File pomFile;
 
     /**
-     * Performs the cleanup.
+     * Transformer.
      */
-    public void execute() throws MojoExecutionException {
+    private final Transformer transformer;
+
+    /**
+     * Initializes the transfomer.
+     */
+    public CleanMojo() {
+        try {
+            final InputStream xsltStream = getClass().getResourceAsStream(
+                    "/META-INF/pom-clean.xslt");
+            transformer = TransformerFactory.newInstance().newTransformer(
+                    new StreamSource(xsltStream));
+            xsltStream.close();
+        } catch (final TransformerConfigurationException e) {
+            throw new IllegalStateException(e);
+        } catch (final IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    /**
+     * Builds the temporary file based on the {@link #pomFile}.
+     *
+     * @return temporary file
+     * @throws MojoExecutionException
+     */
+    private File buildTempFile() throws MojoExecutionException {
         final File tempFile;
         try {
             tempFile = File.createTempFile("pom", "xml");
@@ -55,17 +92,47 @@ public class CleanMojo extends AbstractMojo {
             throw new MojoExecutionException(format(R.getString("copyfail"),
                     pomFile, tempFile), e);
         }
+        return tempFile;
+    }
+
+    /**
+     * Performs the cleanup.
+     *
+     * @throws MojoExecutionException
+     *             problem with the execution.
+     */
+    @Override
+    public void execute() throws MojoExecutionException {
+        final File tempFile = buildTempFile();
+        transform(tempFile, pomFile);
+    }
+
+    /**
+     * Performs the transformation.
+     *
+     * @param sourceFile
+     *            source file
+     * @param targetFile
+     *            target file
+     * @throws MojoExecutionException
+     *             problem with the execution.
+     */
+    private void transform(final File sourceFile, final File targetFile)
+            throws MojoExecutionException {
         try {
-            final Transformer t = TransformerFactory.newInstance()
-                    .newTransformer(
-                            new StreamSource(getClass().getResourceAsStream(
-                                    "/META-INF/pom-clean.xslt")));
-            t.transform(new StreamSource(tempFile), new StreamResult(pomFile));
-            getLog().debug(format(R.getString("donecleaning"), pomFile));
-            tempFile.delete();
+            final OutputStream outputStream = buildContext
+                    .newFileOutputStream(targetFile);
+            transformer.transform(new StreamSource(sourceFile),
+                    new StreamResult(outputStream));
+            getLog().debug(format(R.getString("donecleaning"), targetFile));
+            sourceFile.delete();
+            outputStream.close();
         } catch (final TransformerException e) {
             throw new MojoExecutionException(format(
-                    R.getString("transformfail"), tempFile), e);
+                    R.getString("transformfail"), sourceFile), e);
+        } catch (final IOException e) {
+            throw new MojoExecutionException(format(
+                    R.getString("transformfail"), sourceFile), e);
         }
     }
 }
