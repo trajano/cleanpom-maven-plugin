f93ef2e105a1131bdcc0cc317b9c41692a45c012
Archimedes Trajano <arch@trajano.net>
2015-04-26 19:18:24 -0400

    Added "clean-xml" target

---
+++ src/main/java/net/trajano/mojo/cleanpom/CleanXmlMojo.java
@@ -0,0 +1,157 @@
+package net.trajano.mojo.cleanpom;
+
+import static java.lang.String.format;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ResourceBundle;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.maven.model.FileSet;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.Scanner;
+import org.sonatype.plexus.build.incremental.BuildContext;
+
+/**
+ * Cleans XML sources in general. Tied to the {@link LifecyclePhase#INITIALIZE}
+ * so sources are cleaned up before doing anything else.
+ */
+@Mojo(name = "clean-xml", defaultPhase = LifecyclePhase.INITIALIZE, threadSafe = true)
+public class CleanXmlMojo extends AbstractMojo {
+    /**
+     * Resource bundle.
+     */
+    private static final ResourceBundle R = ResourceBundle.getBundle("META-INF/Messages");
+
+    /**
+     * Build context.
+     */
+    @Component
+    private BuildContext buildContext;
+
+    /**
+     * List of XML files to process. This defaults to
+     * <code>src/main/*.xml</code>.
+     */
+    @Parameter(required = false)
+    private FileSet xmlFiles;
+
+    /**
+     * Builds the handler chain.
+     *
+     * @param tf
+     *            SAX transformer factory
+     * @param outputStream
+     *            output stream
+     * @return handler chain.
+     * @throws IOException
+     *             I/O error when reading the XSLT file.
+     * @throws TransformerException
+     *             problem building the templates.
+     */
+    private TransformerHandler buildHandlerChain(final SAXTransformerFactory tf, final OutputStream outputStream)
+            throws IOException, TransformerException {
+        TransformerHandler handler = null;
+        TransformerHandler lastHandler = null;
+
+        final String[] xsltFiles = new String[] { "/META-INF/clean.xslt" };
+        for (final String xsltFile : xsltFiles) {
+            final InputStream xsltStream = getClass()
+                    .getResourceAsStream(xsltFile.charAt(0) == '/' ? xsltFile : "/META-INF/" + xsltFile);
+            // The stream source needs to be defined here.
+            final TransformerHandler currentHandler = tf.newTransformerHandler(new StreamSource(xsltStream)); // NOPMD
+            xsltStream.close();
+            if (lastHandler != null) {
+                // The result object needs to be created here.
+                lastHandler.setResult(new SAXResult(currentHandler)); // NOPMD
+                lastHandler = currentHandler;
+            } else {
+                lastHandler = currentHandler;
+                handler = currentHandler;
+            }
+        }
+        lastHandler.setResult(new StreamResult(outputStream));
+        return handler;
+    }
+
+    /**
+     * Performs the cleanup.
+     *
+     * @throws MojoExecutionException
+     *             problem with the execution.
+     */
+    @Override
+    public void execute() throws MojoExecutionException {
+        if (xmlFiles == null) {
+            xmlFiles = new FileSet();
+            xmlFiles.setDirectory("src/main");
+            xmlFiles.addInclude("**/*.xml");
+        }
+
+        final File tempFile;
+        try {
+            tempFile = File.createTempFile("pom", "xml");
+        } catch (final IOException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+        final Scanner scanner = buildContext.newScanner(new File(xmlFiles.getDirectory()), false);
+        scanner.setIncludes(xmlFiles.getIncludes().toArray(new String[0]));
+        scanner.scan();
+        for (final String includedFile : scanner.getIncludedFiles()) {
+            final File file = new File(scanner.getBasedir(), includedFile);
+
+            try {
+                FileUtils.copyFile(file, tempFile);
+                transform(tempFile, file);
+            } catch (final IOException e) {
+                throw new MojoExecutionException(format(R.getString("copyfail"), file, tempFile), e);
+            }
+
+        }
+        tempFile.delete();
+    }
+
+    /**
+     * Performs the transformation.
+     *
+     * @param sourceFile
+     *            source file
+     * @param targetFile
+     *            target file
+     * @throws MojoExecutionException
+     *             problem with the execution.
+     */
+    private void transform(final File sourceFile, final File targetFile) throws MojoExecutionException {
+        try {
+            final SAXTransformerFactory tf = (SAXTransformerFactory) TransformerFactory.newInstance();
+
+            final OutputStream outputStream = buildContext.newFileOutputStream(targetFile);
+            final TransformerHandler handler = buildHandlerChain(tf, outputStream);
+            final Transformer transformer = tf.newTransformer();
+            transformer.transform(new StreamSource(sourceFile), new SAXResult(handler));
+            getLog().debug(format(R.getString("donecleaning"), targetFile));
+            sourceFile.delete();
+            outputStream.close();
+        } catch (final TransformerException e) {
+            throw new MojoExecutionException(format(R.getString("transformfail"), sourceFile), e);
+        } catch (final IOException e) {
+            throw new MojoExecutionException(format(R.getString("transformfailio"), sourceFile), e);
+        }
+    }
+}
---
+++ src/test/java/net/trajano/mojo/cleanpom/test/CleanXmlMojoTest.java
@@ -0,0 +1,80 @@
+package net.trajano.mojo.cleanpom.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.model.FileSet;
+import org.apache.maven.plugin.testing.MojoRule;
+import org.codehaus.plexus.util.FileUtils;
+import org.junit.Rule;
+import org.junit.Test;
+
+import net.trajano.mojo.cleanpom.CleanMojo;
+import net.trajano.mojo.cleanpom.CleanXmlMojo;
+
+/**
+ * Tests the {@link CleanMojo}.
+ */
+public class CleanXmlMojoTest {
+    @Rule
+    public MojoRule rule = new MojoRule();
+
+    /**
+     * Tests general cleaning.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGeneralCleaning() throws Exception {
+        final File testPom = new File("src/test/resources/net/trajano/mojo/cleanpom/cleaner-pom.xml");
+        final File dirtyXml = new File("src/test/resources/net/trajano/mojo/cleanpom/dirty.xml");
+        final File cleanedXml = new File("src/test/resources/net/trajano/mojo/cleanpom/cleaned.xml");
+        final File temp = File.createTempFile("dirty", "");
+        temp.delete();
+        temp.mkdirs();
+        FileUtils.copyFile(dirtyXml, new File(temp, "dirty1.xml"));
+        FileUtils.copyFile(dirtyXml, new File(temp, "dirty2.xml"));
+        FileUtils.copyFile(cleanedXml, new File(temp, "dirty3.xml"));
+        assertTrue(temp.exists());
+
+        final CleanXmlMojo mojo = (CleanXmlMojo) rule.lookupMojo("clean-xml", testPom);
+        final FileSet xmlFiles = new FileSet();
+        xmlFiles.setDirectory(temp.getAbsolutePath());
+        xmlFiles.addInclude("**/*.xml");
+        rule.setVariableValueToObject(mojo, "xmlFiles", xmlFiles);
+        assertNotNull(mojo);
+        mojo.execute();
+        final String cleanData;
+        {
+            final FileInputStream fileInputStream = new FileInputStream(cleanedXml);
+            cleanData = IOUtils.toString(fileInputStream);
+            fileInputStream.close();
+
+        }
+        {
+            final FileInputStream fileInputStream = new FileInputStream(new File(temp, "dirty1.xml"));
+            final String data = IOUtils.toString(fileInputStream);
+            fileInputStream.close();
+            assertEquals(cleanData, data);
+        }
+        {
+            final FileInputStream fileInputStream = new FileInputStream(new File(temp, "dirty2.xml"));
+            final String data = IOUtils.toString(fileInputStream);
+            fileInputStream.close();
+            assertEquals(cleanData, data);
+        }
+        {
+            final FileInputStream fileInputStream = new FileInputStream(new File(temp, "dirty3.xml"));
+            final String data = IOUtils.toString(fileInputStream);
+            fileInputStream.close();
+            assertEquals(cleanData, data);
+        }
+        FileUtils.deleteDirectory(temp);
+    }
+
+}
